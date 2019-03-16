bc72c07c65a33c8c67db7298a020f0190f8103db
Lucas Dornelles <lucasdornellesr@gmail.com>
2019-03-16 17:09:19 -0300

    new notInArray test and corrected indexOf

--- src/main/java/com/kata/karatechop/BinarySearch.java
+++ src/main/java/com/kata/karatechop/BinarySearch.java
@@ -4,15 +4,24 @@ public class BinarySearch {
 
     public int indexOf(int item, Integer[] array) {
         int searchIndex = (array.length)/2;
+        int upperIndex = array.length - 1;
         int searchItem = array[searchIndex];
+        int newSearchIndex;
         boolean endOfArray = false;
         while (!endOfArray) {
             if (searchItem > item) {
-                searchIndex = searchIndex/2;
-                searchItem = array[searchIndex];
+                newSearchIndex = (searchIndex)/2;
+                searchItem = array[newSearchIndex];
+                if (newSearchIndex == searchIndex) endOfArray = true;
+                else {
+                    upperIndex = searchIndex;
+                    searchIndex = newSearchIndex;
+                }
             } else if (searchItem < item) {
-                searchIndex = (searchItem + array.length)/2;
-                searchItem = array[searchIndex];
+                newSearchIndex = (searchIndex + upperIndex)/2;
+                searchItem = array[newSearchIndex];
+                if (newSearchIndex == searchIndex) endOfArray = true;
+                else searchIndex = newSearchIndex;
             } else if (searchItem == item) {
                 return searchIndex;
             }
--- src/test/java/com/kata/karatechop/BinarySearchTest.java
+++ src/test/java/com/kata/karatechop/BinarySearchTest.java
@@ -18,11 +18,14 @@ public class BinarySearchTest {
     @Test
     public void longOddArray () {
         Integer[] array = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        assertEquals(0, binarySearch.indexOf(1, array));
-        assertEquals(1, binarySearch.indexOf(2, array));
+        assertEquals("search for 0", 0, binarySearch.indexOf(1, array));
+        assertEquals("search for 1", 1, binarySearch.indexOf(2, array));
+    }
 
-
-
+    @Test
+    public void notInArray () {
+        Integer[] array = new Integer[]{0, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertEquals(-1, binarySearch.indexOf(1, array));
     }
 
 
